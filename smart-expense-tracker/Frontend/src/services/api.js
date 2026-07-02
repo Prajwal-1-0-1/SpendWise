98ada13782fe151bc7ba82b237fe04e4e19f0981
Prajwal <175596070+prajwalys101@users.noreply.github.com>
2026-07-03 04:17:31 +0530

    Trying to connect frontend and backend

--- smart-expense-tracker/Frontend/src/services/api.js
+++ smart-expense-tracker/Frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+console.log(import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '',
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
