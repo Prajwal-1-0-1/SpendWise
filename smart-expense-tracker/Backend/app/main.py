da5507b9e782fad7aadde93e0783efa28e103d64
Prajwal <175596070+prajwalys101@users.noreply.github.com>
2026-07-03 04:32:46 +0530

    fixing...

--- smart-expense-tracker/Backend/app/main.py
+++ smart-expense-tracker/Backend/app/main.py
@@ -4,10 +4,25 @@ from app.models import Base
 from app.analytics import router as analytics_router
 from app.crud import router as crud_router
 from app.auth_routes import router as auth_router
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "https://spend-wise-anvwpmyi3-dinnerbone.vercel.app",  # your Vercel frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 app.include_router(analytics_router)
 app.include_router(auth_router)
 app.include_router(crud_router)
